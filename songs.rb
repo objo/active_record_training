e339562844c36615f3ba1893acbbbbe08337bc03
Joe OBrien <joe@objo.com>
2016-12-07 10:35:54 -0500

    updated logger to show sql being generated

--- songs.rb
+++ songs.rb
@@ -5,6 +5,8 @@ ActiveRecord::Base.establish_connection(
   database: "db/song_list.sqlite"
 )
 
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 class CreateSongsMigration < ActiveRecord::Migration[4.2]
   def change
     create_table :songs do |table|
@@ -16,3 +18,12 @@ end
 
 CreateSongsMigration.migrate(:down)
 CreateSongsMigration.migrate(:up)
+
+
+class Song < ActiveRecord::Base
+end
+
+
+Song.create!({ name: "Allentown", runtime: 432 })
+Song.create!({ name: "Uptown Girl", runtime: 654 })
+Song.create!({ name: "Goodnight Saigon", runtime: 200 })
